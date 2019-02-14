06410914da62fc27b18aaa0c9c6a58012c8cc7c3
Hal Blakeslee <hal@hyprsoft.com>
2019-02-13 19:06:49 -0800

    Updated REST client NuGet.

--- Hyprsoft.Dns.Monitor/deploy.bat
+++ Hyprsoft.Dns.Monitor/deploy.bat
@@ -1,3 +1,5 @@
 dotnet publish -r linux-arm -c Release
 del /s /q d:\shared\publish\*.*
 xcopy .\bin\Release\netcoreapp2.2\linux-arm\publish d:\shared\publish /i /d /y /e
+del d:\shared\publish\appsettings.json
+pause
