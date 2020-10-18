9524773cade23e9977cefaa10b9e19b4d08aa72b
Hal Blakeslee <hal@hyprsoft.com>
2020-10-18 11:01:50 -0700

    Upgrade to .NET Core 3.19 and upgraded Nugets.

--- Hyprsoft.Dns.Monitor/deploy.bat
+++ Hyprsoft.Dns.Monitor/deploy.bat
@@ -1,4 +1,4 @@
 
-dotnet publish -c Release -r linux-arm 
+dotnet publish -c Release -r linux-arm
+del "bin\Release\netcoreapp3.1\linux-arm\publish\appsettings.json"
 dotnet publish -c Release -r linux-x64
-pause
