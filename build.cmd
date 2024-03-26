eabddf45558f372c2ce0930009ec0b72a8a7631c
Hal Blakeslee <hal@hyprsoft.com>
2024-03-25 20:53:28 -0700

    .NET 8 upgrade.

--- build.cmd
+++ build.cmd
@@ -1,12 +1,12 @@
 cd ./Hyprsoft.Dns.Monitor
 
-dotnet publish -c Release
+dotnet publish -c Release -r linux-amd64
 dotnet publish -c Release -r linux-arm64
 
 cd ..
 
-docker build --rm -t hyprsoft/hyprsoft.dns.monitor:1.3.1-linux-amd64 -f Dockerfile.linux-amd64 .
-docker push hyprsoft/hyprsoft.dns.monitor:1.3.1-linux-amd64
+docker build --rm -t hyprsoft/hyprsoft.dns.monitor:1.3.2-linux-amd64 -f Dockerfile.linux-amd64 .
+docker push hyprsoft/hyprsoft.dns.monitor:1.3.2-linux-amd64
 
-docker build --rm -t hyprsoft/hyprsoft.dns.monitor:1.3.1-linux-arm64 -f Dockerfile.linux-arm64 .
-docker push hyprsoft/hyprsoft.dns.monitor:1.3.1-linux-arm64
+docker build --rm -t hyprsoft/hyprsoft.dns.monitor:1.3.2-linux-arm64 -f Dockerfile.linux-arm64 .
+docker push hyprsoft/hyprsoft.dns.monitor:1.3.2-linux-arm64
